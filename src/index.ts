f5d1d63f2958f97a9581fad50faffd191e47c1e6
hp113 <hariprasad2315@gmail.com>
2024-09-10 22:50:12 +0530

    changes in code for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
+import express from 'express';
 
-const wss = new WebSocketServer({ port: 8080 });
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+
+const server = app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+const wss = new WebSocketServer({ server });
 
 const gameManager = new GameManager();
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', (ws)=> {
     gameManager.addUser(ws);
-
-    ws.on("disconnect",() => gameManager.removeUser(ws));
+    ws.on('close', () =>{
+        gameManager.removeUser(ws);
+    })
+    // ws.on("disconnect",() => gameManager.removeUser(ws));
 });
